4097c7df58ba04b119a22be0a48d9e1931fb0249
jthanh8144 <thanhvo618@gmail.com>
2026-01-08 23:39:16 +0700

    fix: remove validate env

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { cert, initializeApp } from 'firebase-admin/app'
 import {
   appProvider,
   databaseProvider,
-  envLoadProvider,
+  // envLoadProvider,
 } from './shared/providers'
 import { firebaseConfig } from './shared/configs/firebase.config'
 ;(async () => {
   initializeApp({
     credential: cert(firebaseConfig as any),
   })
-  envLoadProvider.validate()
+  // envLoadProvider.validate()
   await databaseProvider.initialize()
   appProvider.listen()
 })()
